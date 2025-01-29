4b59d166cb3f04410623eb8c8aeaec3ceb335669
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-29 23:14:34 +0000

    feat: create users

--- src/controllers/UsersControllers.ts
+++ src/controllers/UsersControllers.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import redis from "../lib/cache";
 
 const prisma = new PrismaClient();
 export default class UserController{
     static async find(req:Request, res:Response){
         try{
-            console.time('find Users')
+         const cacheKey = 'users:all';
+         
+         const cachedUsers = await redis.get(cacheKey);
+
+         console.time('find Users')
+         if(cachedUsers){ 
+            console.timeEnd('find Users')
+            // termina o calculo do tempo 
+            return res.json(JSON.parse(cachedUsers))  
+         }
+
+            
            const users = await prisma.user.findMany();
            console.timeEnd('find Users')
+
+         await redis.set(cacheKey, JSON.stringify(users));
+
            return res.json(users)
         }catch(e){
             console.log(e)
